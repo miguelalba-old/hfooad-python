68c374389d8f9edcb3efd655b23753c927042cdb
Miguel Alba <miguelalba89@gmail.com>
2014-04-07 15:17:14 +0200

    Dog door - Remote with timer!

---
+++ ch2/dogdoor1.py
@@ -0,0 +1,53 @@
+""" Dog door - initial version
+
+Author: m1ge7
+Date: 2014/04/07
+"""
+
+from threading import Timer
+
+
+class DogDoor:
+    
+    def __init__(self):
+        self._open = False
+
+    def open(self):
+        print("The dog door opens")
+        self._open = True
+
+    def close(self):
+        print("The dog door closes")
+        self._open = False
+
+    def is_open(self):
+        return self._open
+
+
+class Remote:
+
+    def __init__(self, door):
+        self._door = door
+
+    def press_button(self):
+        print("Pressing the remote control button...")
+        if self._door.is_open():
+            self._door.close()
+        else:
+            self._door.open()
+            timer = Timer(5, lambda door: door.close(), [self._door])
+            timer.start()
+
+
+if __name__ == '__main__':
+    door = DogDoor()
+    remote = Remote(door)
+
+    print("Fido barks to go outside...")
+    remote.press_button()
+
+    print("\nFido has gone outside...")
+
+    print("\nFido's all done...")
+
+    print("\nFido's back inside...")
